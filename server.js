85dd38e915274693b30938589422cd1cd76bfe37
Tomasz Rakowski <tomasz@akayami.com>
2017-03-27 15:07:52 -0400

    added server support

---
+++ server.js
@@ -0,0 +1,25 @@
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+	console.info('Master Process Started - PID: ' + process.pid);
+	// Fork workers.
+	for (var i = 0; i < numCPUs; i++) {
+		var worker = cluster.fork();
+		console.info('Worker Started - PID: ' + worker.process.pid);
+	}
+	cluster.on('exit', function(worker, code, signal) {
+		// Restart the worker
+		var worker = cluster.fork();
+
+		// Note the process IDs
+		var newPID = worker.process.pid;
+		var oldPID = deadWorker.process.pid;
+
+		// Log the event
+		console.warn('worker ' + oldPID + ' died.');
+		console.warn('worker ' + newPID + ' born.');
+	});
+} else {
+	require('./index');
+}
